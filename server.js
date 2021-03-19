8dc4ec203d8d994336d31b8d655883138d6ee183
Amit Kumar <amitkumarmahto76@gmail.com>
2021-03-20 02:05:05 +0530

    server changes

--- server.js
+++ server.js
@@ -1,35 +1,15 @@
-// Get dependencies
+//Install express server
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
-const http = require('http');
-
 
 const app = express();
 
-// allow cross origin
-app.use(cors());
-// Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/angular'));
 
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/angular/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/angular/'}),
+);
 
-/**
- * Get port from environment and store in Express.
- */
-// const port = config.get('serverPort');
-// app.set('port', 8081);
-// // app.listen(process.env.PORT || 8080);
-// /**
-//  * Create HTTP server.
-//  */
-// const server = http.createServer(app);
-
-// /**
-//  * Listen on provided port, on all network interfaces.
-//  */
-// server.listen(port, () => console.log(`API running on ${config.get('hostAddress')}:${port}`));
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
